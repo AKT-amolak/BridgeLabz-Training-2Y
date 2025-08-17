1280931b6e25277e0c991c8703faffdfcdd6a158
Amolak <amolaktaliwal@gmail.com>
2025-08-17 23:54:40 +0530

    problem

---
+++ Day2_ProgrammingElements/Level 1/KM.java
@@ -0,0 +1,9 @@
+public class KM {
+     public static void main(String[] args) {
+        double Km = 10.8;
+
+        double Miles = Km * 0.62;
+
+        System.out.println("The distance "+ Km +"in miles is "+ Miles);
+    }
+}
